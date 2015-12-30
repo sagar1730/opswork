45a366343cabbfb1b623978f2ec25d3837b7f12b
Sagar <sagar.empire1730@gmail.com>
2015-12-30 12:51:42 +0530

    IP cookbook deleted

--- cookbooks/ip/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'ip'
-maintainer       'Minjar Cloud Solutions'
-maintainer_email 'sharvil.parekh@minjar.com'
-license          'All rights reserved'
-description      'Installs/Configures filebeat_minjar'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
