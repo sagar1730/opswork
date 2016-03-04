606daef49ed7d6a474510c459569f24ede96d34b
Sagar <sagar.ghuge@minjar.com>
2016-03-04 12:47:13 +0530

    Kafka, ES, Logstash cookbooks removed

--- cookbooks/elasticsearch/metadata.rb
+++
@@ -1,9 +0,0 @@
-name 'elasticsearch'
-maintainer 'Minjar'
-maintainer_email 'sagar.ghuge@minjar.com'
-license 'Apache 2.0'
-description 'Installs/Configures ElasticSearch Cluster'
-long_description 'See the readme'
-version '0.5.2'
-
-depends 'java'
--- cookbooks/kafka/files/default/kafka_configure.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cd /home/ubuntu/kafka/kafka* && sudo bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 3 --partitions 1 --topic minjar > /home/ubuntu/kafka/kafka_configure.log
